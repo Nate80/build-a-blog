339d14c992f3b811d2729879c8e4cb9e8f34cf2a
Nathan Mohr <nlm9380@gmail.com>
2016-09-13 11:41:57 -0500

    Add new changes

--- main.py
+++ main.py
@@ -14,12 +14,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class NewPost(db.Model):
+    title = db.StringProperty(required = True)
+    newpost = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    modified = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainBlogHandler(Handler):
+    def render_base(self, title="", newpost="", error=""):
+        newposts = db.GqlQuery("SELECT * FROM NewPost "
+                                "ORDER BY created DESC",
+                                "LIMIT 5")
+
+        self.render("base.html", title=title, newpost=newpost, error=error, newposts=newposts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_base()
+
+    def post(self):
+        title = self.request.get("title")
+        newpost = self.request.get("newpost")
+
+        if title and newpost:
+            np = NewPost(title = title, newpost = newpost)
+            np.put()
+
+            self.redirect("/")
+        else:
+            error = "We need a title and an entry!"
+            self.render_base(title, newpost, error)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainBlogHandler),
 ], debug=True)
